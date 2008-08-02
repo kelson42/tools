51c5b001678d349ee58048741d0104a583981279
kelson42 <kelson42@users.sourceforge.net>
2008-08-02 17:51:04 +0000

    + usability improvments

--- dumping_tools/scripts/buildZenoFileFromDirectory.pl
+++ dumping_tools/scripts/buildZenoFileFromDirectory.pl
@@ -9,9 +9,7 @@ use warnings;
 use Getopt::Long;
 use Data::Dumper;
 use Kiwix::ZenoIndexer;
-
-# sqlite DB path
-my $dbFile="/tmp/dbname=".time();
+use Whereis;
 
 # log
 use Log::Log4perl;
@@ -21,18 +19,49 @@ my $logger = Log::Log4perl->get_logger("builZenoFileFromDirectory.pl");
 # get the params
 my $indexerPath;
 my $htmlPath;
-my $zenoFilePath;
-my $textCompression="none";
+my $zenoFilePath="./articles.zeno";
+my $textCompression="gzip";
+my $tmpDirectoryPath;
 
 # Get console line arguments
 GetOptions('indexerPath=s' => \$indexerPath, 
 	   'htmlPath=s' => \$htmlPath,
 	   'zenoFilePath=s' => \$zenoFilePath,
-	   'textCompression=s' => \$textCompression
+	   'textCompression=s' => \$textCompression,
+	   'tmpDirectoryPath=s' => \$tmpDirectoryPath
 	   );
 
-if (!$indexerPath || !$htmlPath || !$zenoFilePath) {
-    print "usage: ./builZenoFileFromDirectory.pl --indexerPath=./zenoindexer --htmlPath=./html --zenoFilePath=articles.zeno [--textCompression={none|gzip}]\n";
+if (!$htmlPath) {
+    print "usage: ./builZenoFileFromDirectory.pl --htmlPath=./html [--indexerPath=./zenoindexer] [--zenoFilePath=articles.zeno] [--textCompression={none|gzip}] [--tmpDirectoryPath=/tmp]\n";
+    exit;
+}
+
+# sqlite DB path
+my $dbFile;
+if ($tmpDirectoryPath && -w $tmpDirectoryPath ) {
+    $dbFile= $tmpDirectoryPath."/.dbname=".time();
+} elsif (-w "/tmp") {
+    $dbFile="/tmp/.dbname=".time();
+} elsif (-w "./") {
+    $dbFile="./.dbname=".time();
+} else {
+    $logger->error("You need a writable temp directory, please specify one.");
+    exit;
+}
+
+# try to detect the zenoindexer path or test it (if given)
+if ($indexerPath) {
+    unless (-x $indexerPath) {
+	$logger->error("The zeno indexer '$indexerPath' does not exist or is not executable.");
+	exit;
+    }
+} else {
+    $indexerPath = whereis("zenowriter");
+}
+
+# test the html directory
+unless (-d $htmlPath) {
+    $logger->error("The html directory '$htmlPath' does not exist.");
     exit;
 }
 
@@ -47,9 +76,9 @@ $indexer->textCompression($textCompression);
 # loads the data from the directory to the db
 $indexer->exploreHtmlPath();
 $indexer->buildDatabase($dbFile);
-
+exit;
 # call the zeno indexer
-`$indexerPath --db "sqlite:$dbFile" wikipedia.zeno`;
+`$indexerPath --db "sqlite:$dbFile" $zenoFilePath`;
 
 # delete temporary dbFile
 if ( unlink($dbFile) == 0 ) {
