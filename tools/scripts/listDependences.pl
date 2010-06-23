05d1cf16fd73653fdd50fedc8cf32a1795eeb6d2
kelson42 <kelson42@users.sourceforge.net>
2010-06-23 11:18:26 +0000

    + class renaming

--- tools/scripts/listDependences.pl
+++ tools/scripts/listDependences.pl
@@ -3,16 +3,14 @@ binmode STDOUT, ":utf8";
 binmode STDIN, ":utf8";
 
 use utf8;
-
-use lib "../";
-use lib "../Mediawiki/";
+use lib "../classes/";
 
 use DBI;
 use strict;
 use warnings;
 use Getopt::Long;
 use Data::Dumper;
-use MediaWiki;
+use Mediawiki::Mediawiki;
 use Encode;
 
 # log
@@ -89,7 +87,7 @@ if ($databaseName) {
     }
 
 } else {
-    my $site = MediaWiki->new();
+    my $site = Mediawiki::Mediawiki->new();
     $site->hostname($host);
     $site->path($path);
     $site->logger($logger);
