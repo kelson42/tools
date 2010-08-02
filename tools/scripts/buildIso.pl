67278afbe2e9307e1f76f1172276f2dc80eef318
kelson42 <kelson42@users.sourceforge.net>
2010-08-02 13:18:17 +0000

    ++

--- tools/scripts/buildIso.pl
+++ tools/scripts/buildIso.pl
@@ -49,5 +49,54 @@ $logger->info("Checkout the SVN dvd directory template");
 $cmd = "svn co https://kiwix.svn.sourceforge.net/svnroot/kiwix/moulinkiwix/dvd $isoDirectory"; `$cmd`;
 $cmd = "rm -rf \`find $isoDirectory -name \"*.svn\"\`"; `$cmd`;
 
+# Copy the ZIMs
+$logger->info("Copying the ZIM files");
+foreach my $zimPath (@zimPaths) {
+    $logger->info("Check ZIM file $zimPath");
+    $cmd = "cp $zimPath $isoDirectory/data/content/"; `$cmd`;
+}
+
+# Update @zimPaths
+my $output = `find /tmp/kiwix_iso_tmp_directory/data/content/ -name \"*.zim\" 2>&1`;
+@zimPaths = split (/\n/, $output);
+
+# Compute and compact the indexes
+$logger->info("Compute and compact the indexes");
+foreach my $zimPath (@zimPaths) {
+
+    # Extract the zimFile
+    $zimPath =~ /.*\/([^\/]*)$/;
+    my $zimFile = $1;
+
+    # Index
+    my $zimFileIndex = "$isoDirectory/data/index/$zimFile.idx";
+    $logger->info("Compute index for $zimFile");
+    $cmd = "kiwix-index $zimPath $zimFileIndex"; `$cmd`;
+
+    # Compact
+    $logger->info("Compact index $zimFileIndex");
+    $cmd = "kiwix-compact $zimFileIndex"; `$cmd`;
+
+    # Compute the zimId
+    $cmd = "zimdump -F /tmp/kiwix_iso_tmp_directory/data/content/ubuntudoc_fr_01_2009.zim  | grep uuid | sed 's/uuid: //'";
+    my $zimFileId = `$cmd 2>&1`;
+    $zimFileId =~ s/\r|\n//g;
+
+    # Create the library
+    my $libraryPath = "$isoDirectory/data/library/$zimFile.library";
+    $logger->info("Create the library for $zimFile : ");
+    my $xmlContent = "<library current=\"$zimFileId\"><book id=\"$zimFileId\" path=\"$zimFile\" indexPath=\"$zimFile.idx\" indexType=\"xapian\"/></library>\n";
+    writeFile($libraryPath, $xmlContent);
+}
+
+sub writeFile {
+    my $file = shift;
+    my $data = shift;
+    
+    open (FILE, ">:utf8", "$file") or die "Couldn't open file: $file";
+    print FILE $data;
+    close (FILE);
+}
+
 exit 0;
 
