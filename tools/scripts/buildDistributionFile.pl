ff48405b34afcbdde3804b2389e9747d1a0d553f
kelson42 <kelson42@users.sourceforge.net>
2010-08-29 15:13:05 +0000

    ++

--- tools/scripts/buildDistributionFile.pl
+++ tools/scripts/buildDistributionFile.pl
@@ -28,7 +28,7 @@ GetOptions('zimPath=s' => \@zimPaths,
     );
 
 # Check if we have all the mandatory variable set
-if (!scalar(@zimPaths) || !$filePath || ($type != "iso" && $type != "portable")) {
+if (!scalar(@zimPaths) || !$filePath || (!($type eq "iso") && !($type eq "portable"))) {
     print "usage: ./buildIso.pl --filePath=dvd.iso --zimPath=articles.zim --type=[iso|portable] [--tmpDirectory=/tmp/] [--lang=en|fr]\n";
     exit
 }
@@ -43,21 +43,21 @@ foreach my $zimPath (@zimPaths) {
 }
 
 # Clear and create the directory to build the iso
-my $isoDirectory = $tmpDirectory."kiwix_iso_tmp_directory/";
-$logger->info("Deleting and creating $isoDirectory");
-$cmd = "rm -rf $isoDirectory"; `$cmd`; 
-$cmd = "mkdir $isoDirectory"; `$cmd`; 
+my $distributionDirectory = $tmpDirectory."kiwix_iso_tmp_directory/";
+$logger->info("Deleting and creating $distributionDirectory");
+$cmd = "rm -rf $distributionDirectory"; `$cmd`; 
+$cmd = "mkdir $distributionDirectory"; `$cmd`; 
 
 # Checkout the default ISO directory tree structure
 $logger->info("Checkout the SVN dvd directory template");
-$cmd = "svn co https://kiwix.svn.sourceforge.net/svnroot/kiwix/moulinkiwix/dvd $isoDirectory"; `$cmd`;
-$cmd = "rm -rf \`find $isoDirectory -name \"*.svn\"\`"; `$cmd`;
+$cmd = "svn co https://kiwix.svn.sourceforge.net/svnroot/kiwix/moulinkiwix/dvd $distributionDirectory"; `$cmd`;
+$cmd = "rm -rf \`find $distributionDirectory -name \"*.svn\"\`"; `$cmd`;
 
 # Copy the ZIMs
 $logger->info("Copying the ZIM files");
 foreach my $zimPath (@zimPaths) {
     $logger->info("Check ZIM file $zimPath");
-    $cmd = "cp $zimPath $isoDirectory/data/content/"; `$cmd`;
+    $cmd = "cp $zimPath $distributionDirectory/data/content/"; `$cmd`;
 }
 
 # Update @zimPaths
@@ -73,7 +73,7 @@ foreach my $zimPath (@zimPaths) {
     my $zimFile = $1;
 
     # Index
-    my $zimFileIndex = "$isoDirectory/data/index/$zimFile.idx";
+    my $zimFileIndex = "$distributionDirectory/data/index/$zimFile.idx";
     $logger->info("Compute index for $zimFile");
     $cmd = "kiwix-index $zimPath $zimFileIndex"; `$cmd`;
 
@@ -88,7 +88,7 @@ foreach my $zimPath (@zimPaths) {
 
     # Create the library
     $logger->info("Create new library file ro $zimFile.");
-    my $libraryPath = "$isoDirectory/data/library/$zimFile.library";
+    my $libraryPath = "$distributionDirectory/data/library/$zimFile.library";
     $logger->info("Create the library for $zimFile : ");
     my $xmlContent = "<library current=\"$zimFileId\"><book id=\"$zimFileId\" path=\"$zimFile\" indexPath=\"$zimFile.idx\" indexType=\"xapian\"/></library>\n";
     writeFile($libraryPath, $xmlContent);
@@ -96,7 +96,7 @@ foreach my $zimPath (@zimPaths) {
 
  # Download the source code
 $logger->info("Download Kiwix source code");
-$cmd = "cd $isoDirectory ; wget \`curl --silent http://sourceforge.net/projects/kiwix/ | grep \"/download\" | sed 's/\\?.*//' | sed 's/.*http/http/'\`"; `$cmd`;
+$cmd = "cd $distributionDirectory ; wget \`curl --silent http://sourceforge.net/projects/kiwix/ | grep \"/download\" | sed 's/\\?.*//' | sed 's/.*http/http/'\`"; `$cmd`;
 
 # Download deb files
 $logger->info("Download Kiwix deb packages");
@@ -104,31 +104,34 @@ $cmd = "curl --silent http://ppa.launchpad.net/kiwixteam/ppa/ubuntu/pool/main/k/
 my @debFiles = split(/\n/, `$cmd 2>&1`);
 
 foreach my $debFile (@debFiles) {
-    $cmd = "wget http://ppa.launchpad.net/kiwixteam/ppa/ubuntu/pool/main/k/kiwix/$debFile -O $isoDirectory/install/$debFile"; `$cmd`;
+    $cmd = "wget http://ppa.launchpad.net/kiwixteam/ppa/ubuntu/pool/main/k/kiwix/$debFile -O $distributionDirectory/install/$debFile"; `$cmd`;
 }
 
 # Download the file with DLL for Visual Studio redit. binaries
 $logger->info("Download vcredist_x86.exe");
-$cmd = "wget http://download.kiwix.org/dev/vcredist_x86.exe -O $isoDirectory/bonus/vcredist_x86.exe"; `$cmd`;
+$cmd = "wget http://download.kiwix.org/dev/vcredist_x86.exe -O $distributionDirectory/bonus/vcredist_x86.exe"; `$cmd`;
 
 # Download and unzip Windows binary
 $logger->info("Download and unzip Windows binary");
-$cmd = "wget \`curl --silent --user-agent \"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.5) Gecko/20041202 Firefox/1.0\" http://sourceforge.net/projects/kiwix/ | grep \"/download\" |  sed 's/\\?.*//' | sed 's/.*http/http/'\` -O $isoDirectory/kiwix.zip"; `$cmd`;
-$cmd = "cd $isoDirectory/ ; unzip -n kiwix.zip" ; `$cmd`;
-$cmd = "rm $isoDirectory/kiwix.zip" ; `$cmd`;
+$cmd = "wget \`curl --silent --user-agent \"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.5) Gecko/20041202 Firefox/1.0\" http://sourceforge.net/projects/kiwix/ | grep \"/download\" |  sed 's/\\?.*//' | sed 's/.*http/http/'\` -O $distributionDirectory/kiwix.zip"; `$cmd`;
+$cmd = "cd $distributionDirectory/ ; unzip -n kiwix.zip" ; `$cmd`;
+$cmd = "rm $distributionDirectory/kiwix.zip" ; `$cmd`;
 
 # Download the autorun
 $logger->info("Download autorun");
-$cmd = "cd $isoDirectory/autorun/ ; wget http://download.kiwix.org/dev/launcher/mingwm10.dll"; `$cmd`;
-$cmd = "cd $isoDirectory/autorun/ ; wget http://download.kiwix.org/dev/launcher/libgcc_s_dw2-1.dll"; `$cmd`;
-$cmd = "cd $isoDirectory/autorun/ ; wget http://download.kiwix.org/dev/launcher/QtGui4.dll"; `$cmd`;
-$cmd = "cd $isoDirectory/autorun/ ; wget http://download.kiwix.org/dev/launcher/QtCore4.dll"; `$cmd`;
-$cmd = "cd $isoDirectory/autorun/ ; wget http://download.kiwix.org/dev/launcher/autorun-$lang.exe -O autorun.exe"; `$cmd`;
+$cmd = "cd $distributionDirectory/autorun/ ; wget http://download.kiwix.org/dev/launcher/mingwm10.dll"; `$cmd`;
+$cmd = "cd $distributionDirectory/autorun/ ; wget http://download.kiwix.org/dev/launcher/libgcc_s_dw2-1.dll"; `$cmd`;
+$cmd = "cd $distributionDirectory/autorun/ ; wget http://download.kiwix.org/dev/launcher/QtGui4.dll"; `$cmd`;
+$cmd = "cd $distributionDirectory/autorun/ ; wget http://download.kiwix.org/dev/launcher/QtCore4.dll"; `$cmd`;
+$cmd = "cd $distributionDirectory/autorun/ ; wget http://download.kiwix.org/dev/launcher/autorun-$lang.exe -O autorun.exe"; `$cmd`;
 
 # Build ISO
 if ($type eq "iso") {
     $logger->info("Build ISO $filePath");
-    $cmd = "mkisofs -r -J -o  $filePath $isoDirectory"; `$cmd`;
+    $cmd = "mkisofs -r -J -o  $filePath $distributionDirectory"; `$cmd`;
+} else { # portable
+    $logger->info("Build the portable compacted file");
+    $cmd = "7za a -tzip -mx9 $filePath $distributionDirectory/*"; `$cmd`;
 }
 
 sub writeFile {
