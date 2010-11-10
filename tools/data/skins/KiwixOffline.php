9589909b4b99bcc9bb104bcf050a1b34b898fa14
kelson42 <kelson42@users.sourceforge.net>
2010-11-10 11:41:24 +0000

    + improve the <body> attribute management to avoid the issue with to much padding on the left with external links. ltr/rtl CSS class is now always specified

--- tools/data/skins/KiwixOffline.php
+++ tools/data/skins/KiwixOffline.php
@@ -73,8 +73,10 @@ class KiwixOfflineTemplate extends QuickTemplate {
 <?php	}
 		if($this->data['trackbackhtml']) print $this->data['trackbackhtml']; ?>
 	</head>
-<body style="margin: 0 1em 1em 1em; background: #FFFFFF" >
-	<div id="globalWrapper"><div id="bodyContent">
+<body<?php if($this->data['body_ondblclick']) { ?> ondblclick="<?php $this->text('body_ondblclick') ?>"<?php } ?>
+<?php if($this->data['body_onload']) { ?> onload="<?php $this->text('body_onload') ?>"<?php } ?>
+ class="mediawiki <?php $this->text('dir') ?> <?php $this->text('pageclass') ?> <?php $this->text('skinnameclass') ?>" style="margin: 0 1em 1em 1em; background: #FFFFFF">
+OA	<div id="globalWrapper"><div id="bodyContent">
 		<a name="top" id="top"></a>
 		<h1 class="firstHeading"><?php $this->html('title'); ?></h1>
 			<?php $this->html('bodytext') ?>
