3c55aa782b32c02624e573f5e1ee197d9d4ac682
kelson42 <kelson42@users.sourceforge.net>
2011-08-04 11:33:34 +0000

    + improvments... dev continues

--- tools/scripts/getTWLocalization.pl
+++ tools/scripts/getTWLocalization.pl
@@ -10,7 +10,7 @@ use strict;
 use warnings;
 use Getopt::Long;
 use Data::Dumper;
-use Mediawiki::Mediawiki;
+use LWP::Simple;
 use Term::Query qw(query);
 
 # log
@@ -22,18 +22,15 @@ my $logger = Log::Log4perl->get_logger("getTWLocalization.pl");
 my @languages;
 my $allLanguages;
 my $path;
-my $source;
-my $rev;
-my $cmd;
 
-## Get console line arguments
+# Get console line arguments
 GetOptions('language=s' => \@languages, 
 	   'path=s' => \$path,
 	   'allLanguages' => \$allLanguages
 	   );
 
 if (!$path) {
-    print STDERR "usage: ./getTWLocalization.pl --path=./ [--language=en-US] [--allLanguages]\n";
+    print STDERR "usage: ./getTWLocalization.pl --path=./ [--language=fr] [--allLanguages]\n";
     exit;
 } elsif (! -d $path || ! -d $path."/kiwix/") {
     print STDERR "'$path' is not a directory, does not exist or is not the Kiwix source directory 'moulinkiwix'.\n";
@@ -49,92 +46,58 @@ if (!scalar(@languages) && !$allLanguages) {
     }
 }
 
-# Initiate the Mediawiki object
-my $site = Mediawiki::Mediawiki->new();
-$site->hostname("www.kiwix.org");
-$site->path("");
-$site->logger($logger);
-
 # Get all languages if necessary
 if (!scalar(@languages) || $allLanguages) {
-    my @embeddedIns = $site->embeddedIn("template:Language_translation", 0);
-    foreach my $embeddedIn (@embeddedIns) {
-	if ($embeddedIn =~ /Translation\/languages\/(.*)/ ) {
-	    my $language = $1;
-	    push(@languages, $language);
-	}
-    }
+    @languages = ("ar", "ca", "de", "es", "fa", "fr", "he". "it", "ml", "nl", "pl", "pt", "zh");
 }
+
+# Initialize $languageMainDtdSourceMaster
+my $languageMainDtdSourceMaster = readFile($path."/kiwix/chrome/locale/en/main/main.dtd");
 
 # Get all languages
 foreach my $language (@languages) {
-
     $logger->info("Getting locale '$language'.");
     
-    # create directory
+    # get translation from translatewiki.net
+    my $content = get("http://sandbox.translatewiki.net/w/i.php?title=Special:Translate&task=export-to-file&group=out-kiwix&language=$language&limit=2500");
+
+    # Update main dtd
+    my $mainDtdHash = getLocaleHash($content, "ui\.|main");
+    my $languageMainDtdSource = $languageMainDtdSourceMaster;
+    while ($languageMainDtdSourceMaster =~ /(!ENTITY[ |\t]+)(.*?)([ |\t]+\")(.*?)(\")/g ) {
+	my $prefix = $1.$2.$3;
+	my $postfix = $5;
+	my $name = $2;
+	my $value = $4;
+
+	if (exists($mainDtdHash->{$name})) {
+	    $value = $mainDtdHash->{$name};
+	}
+
+	$languageMainDtdSource =~ s/\Q$1$2$3$4$5\E/$prefix$value$postfix/;
+    }
+    print $languageMainDtdSource;
+
+    # Upload dtd
+
+    # create the js
+
+    # create directory if necessary
     unless ( -d $path."/kiwix/chrome/locale/".$language) { mkdir $path."/kiwix/chrome/locale/".$language; }
     unless ( -d $path."/kiwix/chrome/locale/".$language."/main") { mkdir $path."/kiwix/chrome/locale/".$language."/main"; }
     my $localePath = $path."/kiwix/chrome/locale/".$language."/main/";
-    my $installerPath = $path."/installer/translations/";    
+}
 
-    # get help.html
-    ($source) = $site->downloadPage("Translation/languages/".$language."/help.html");
-    if ($source) {
-	$source =~ s/^<[\/]*source[^>]*>[\n]*//mg;
-	writeFile($localePath."help.html", $source);
-    }
-    
-    # get main.dtd
-    ($source) = $site->downloadPage("Translation/languages/".$language."/main.dtd");
-    if ($source) {
-	$source =~ s/^<[\/]*source[^>]*>[\n]*//mg;
-	writeFile($localePath."main.dtd", $source);
-    }
-    
-    # get main.properties
-    ($source) = $site->downloadPage("Translation/languages/".$language."/main.properties");
-    if ($source) {
-	$source =~ s/^<[\/]*source[^>]*>[\n]*//mg;
-	writeFile($localePath."main.properties", $source);
-    }
-    
-    # get installer
-    ($source) = $site->downloadPage("Translation/languages/".$language."/installer");
-    if ($source) {
-	$source =~ s/^<[\/]*source[^>]*>[\n]*//mg;
-	my $filename;
-	my $codepage;
+sub getLocaleHash {
+    my $content = shift;
+    my ($prefixEx, $prefixInc) = split(/\|/, shift);
 
-	if ($source =~ /\!define LANG_FILENAME "([^\"]*)"/) {
-	    $filename = $1;
-	}
-
-	if ($source =~ /\!define LANG_CODEPAGE "([^\"]*)"/) {
-	    $codepage = $1;
-	}
-
-	if ($filename && $codepage) {
-	    $source =~ s/^.*(LANG_CODEPAGE|LANG_FILENAME).*$//mg;
-	    $filename = $installerPath.$filename;
-	    my $tmpFilename = $filename.".utf8";
-
-	    writeFile($tmpFilename, $source);
-	    $cmd = "iconv -f UTF-8 -t $codepage $tmpFilename > $filename"; `$cmd`;
-	    $cmd = "rm $tmpFilename"; `$cmd`;
-	}
+    my %translationHash;
+    while ($content =~ /$prefixEx($prefixInc.*)=(.*)/g ) {
+	$translationHash{$1} = $2;
     }
 
-    # get the autorun
-    ($source) = $site->downloadPage("Translation/languages/".$language."/dvd_launcher.xml");
-    if ($source) {
-	$source =~ s/^<[\/]*source[^>]*>[\n]*//mg;
-	my $filename = $language.".xml";
-	$filename =~ s/-[^\.]+//;
-
-	if ($filename) {
-	    writeFile($path."/autorun/ui/".$filename, $source);
-	}
-    }
+    return \%translationHash;
 }
 
 sub writeFile {
