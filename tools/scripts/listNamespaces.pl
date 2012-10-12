66f28318cd4a76a2bcc714149d1dbb29a8343e17
kelson42 <kelson42@users.sourceforge.net>
2012-10-12 16:55:57 +0000

    + fix

--- tools/scripts/listNamespaces.pl
+++ tools/scripts/listNamespaces.pl
@@ -4,14 +4,14 @@ binmode STDIN, ":utf8";
 
 use utf8;
 use lib "../";
-use lib "../Mediawiki/";
+use lib "../classes/";
 
 use Config;
 use strict;
 use warnings;
 use Getopt::Long;
 use Data::Dumper;
-use MediaWiki;
+use Mediawiki::Mediawiki;
 
 # log
 use Log::Log4perl;
@@ -21,21 +21,30 @@ my $logger = Log::Log4perl->get_logger("listNamespaces.pl");
 # get the params
 my $host = "";
 my $path = "";
+my $username = "";
+my $password = "";
 
 ## Get console line arguments
 GetOptions('host=s' => \$host, 
-	   'path=s' => \$path
+	   'path=s' => \$path,
+	   'username=s' => \$username,
+	   'password=s' => \$password
 	   );
 
 if (!$host) {
-    print "usage: ./listNamespaces.pl --host=my.wiki.org [--path=w]\n";
+    print "usage: ./listNamespaces.pl --host=my.wiki.org [--path=w] [--username=foo] [--password=bar]\n";
     exit;
 }
 
-my $site = MediaWiki->new();
+my $site = Mediawiki::Mediawiki->new();
 $site->hostname($host);
 $site->path($path);
 $site->logger($logger);
+if ($username) {
+    $site->user($username);
+    $site->password($password);
+}
+$site->setup();
 
 my %namespaces = $site->namespaces();
 
