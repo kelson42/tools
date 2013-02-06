d2e0760981e37b819c4b96e41d97b0e106a2f66b
kelson42 <kelson42@users.sourceforge.net>
2013-02-06 19:37:34 +0000

    + first version

--- tools/scripts/downloadC2C.pl
+++ tools/scripts/downloadC2C.pl
@@ -7,13 +7,16 @@ use warnings;
 use Getopt::Long;
 use Data::Dumper;
 use HTML::Template;
-use File::Basename;
+use HTML::Entities;
+use LWP::UserAgent;
+use Encode;
 
 my $directory;
 my $help;
 my $delay = 0;
 my $verbose;
 my $templateCode = "";
+my $ua = LWP::UserAgent->new();
 
 sub usage() {
     print "downloadC2C.pl is a script to download free pictures of http://www.camptocamp.org/.\n";
@@ -44,6 +47,70 @@ if (!$directory) {
 
 unless (-d $directory) {
     die "'$directory' seems not to be a valid directory.";
+}
+
+# Go through all images
+my $lastResultPageIndex;
+my $resultPageIndex = 1;
+my $resultPage;
+my $infoPage;
+my $req;
+my $res;
+do { 
+    $resultPage = getUrlContent("http://www.camptocamp.org/images/list/ityp/1/page/$resultPageIndex");
+    while ($resultPage =~ /\/images\/(\d+)\/fr\//gm) {
+	my $uid = $1;
+	my $more = "http://www.camptocamp.org/images/$uid";
+	my $title;
+	my $url;
+	my $author;
+
+	# Get detailed page
+	$infoPage = getUrlContent($more);
+
+	# Get title
+	if ($infoPage =~ /id="article_title".*?><a.*?>(.*?)<\/a>/) {
+	    $title = encode_utf8(decode_entities($1));
+	}
+	
+	# Get url
+	if ($infoPage =~ /href="(http:\/\/s.camptocamp.org\/uploads\/images\/.*?)" itemprop="contentUrl"/) {
+	    $url = $1;
+	}
+
+	# Get author
+	if ($infoPage =~ /itemprop="author" href="\/users\/\d+">(.*?)<\/a>/) {
+	    $author = $1;
+	}
+
+	printLog("UID: $uid");
+	printLog("MORE: $more");
+	printLog("TITLE: $title");
+	printLog("URL: $url");
+	printLog("AUTHOR: $author");
+    }
+
+    # Get $lastResultPageIndex
+    if ($resultPageIndex == 1) {
+	if ($resultPage =~ /\/images\/list\/ityp\/1\/page\/(\d+)"><span class=\"picto action_last/) {
+	    $lastResultPageIndex = $1;
+	} else {
+	    die("Unable to find lastResultPageIndex.")
+	}
+    }
+} while ($resultPageIndex++ < $lastResultPageIndex);
+
+# Request url
+sub getUrlContent {
+    my $url = shift;
+
+    $req = HTTP::Request->new(GET => $url);
+    $res = $ua->request($req);
+    if ($res->is_success) {
+	return $res->content;
+    } else {
+	die("Unable to request '$url': ".$res->status_line);
+    }
 }
 
 # Read/Write functions
