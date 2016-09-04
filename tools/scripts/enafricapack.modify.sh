90073bd523e58bca7dd890f5491ba897e1f6f1d1
kelson42 <kelson@kiwix.org>
2016-09-04 13:04:28 +0200

    imp

--- tools/scripts/enafricapack.modify.sh
+++ tools/scripts/enafricapack.modify.sh
@@ -12,20 +12,23 @@ do
     ./modifyMediawikiEntry.pl --host=en.africapack.kiwix.org --path=w --entry="$ENTRY" --action=replace --variable='{{TNT\|[^}]+}}' --variable=' ' --username=admin --password=adminadmin
     ./modifyMediawikiEntry.pl --host=en.africapack.kiwix.org --path=w --entry="$ENTRY" --action=replace --variable='<tvar\|[^>]+>(Special:MyLanguage\/|)([^>]+)</>' --variable='$2' --username=admin --password=adminadmin
     ./modifyMediawikiEntry.pl --host=en.africapack.kiwix.org --path=w --entry="$ENTRY" --action=replace --variable='\[\[(C|c)ategory:[^]]+\]\]' --variable=' ' --username=admin --password=adminadmin
-
     ./modifyMediawikiEntry.pl --host=en.africapack.kiwix.org --path=w --entry="$ENTRY" --action=replace --variable='\n[ ]+' --variable="\n" --username=admin --password=adminadmin
     ./modifyMediawikiEntry.pl --host=en.africapack.kiwix.org --path=w --entry="$ENTRY" --action=replace --variable='\n+' --variable="\n" --username=admin --password=adminadmin
 done
 
 # Modifications on encyclopedic articles
-./listAllPages.pl --host=en.africapack.kiwix.org --path=w --namespace=0 --namespace=0 > en.all
-./listAllPages.pl --host=en.africapack.kiwix.org --path=w --namespace=0 --namespace=4 >> en.all
-./listAllPages.pl --host=en.africapack.kiwix.org --path=w --namespace=0 --namespace=12 >> fr.all
-./listAllPages.pl --host=en.africapack.kiwix.org --path=w --namespace=0 --namespace=3000 >> fr.all
+./listAllPages.pl --host=en.africapack.kiwix.org --path=w --namespace=0 > en.all
+./listAllPages.pl --host=en.africapack.kiwix.org --path=w --namespace=4 >> en.all
+./listAllPages.pl --host=en.africapack.kiwix.org --path=w --namespace=12 >> fr.all
+./listAllPages.pl --host=en.africapack.kiwix.org --path=w --namespace=3000 >> fr.all
 for ENTRY in `cat en.all`
 do
     ./modifyMediawikiEntry.pl --host=en.africapack.kiwix.org --path=w --entry="$ENTRY" --action=replace --variable='^<!--2BREINSERTED(.*)-->$' --variable='$1' --username=admin --password=adminadmin
-Category:WikiProject_Wikipack_Africa_Content
+done
+
+# Remove project category
+for ENTRY in `./listCategoryEntries.pl --host=en.africapack.kiwix.org --path=w --category=WikiProject_Wikipack_Africa_Content`
+do
     ./modifyMediawikiEntry.pl --host=en.africapack.kiwix.org --path=w --entry="$ENTRY" --action=replace --variable='\[\[Category:WikiProject( |_)Wikipack( |_)Africa( |_)Content\]\]' --variable=' ' --username=admin --password=adminadmin
 done
 
